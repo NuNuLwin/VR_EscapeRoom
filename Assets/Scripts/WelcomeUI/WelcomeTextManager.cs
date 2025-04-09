74d06683e60018a925a8b0f5ac82bac58ec4d9c1
NuNuLwin <cws.nunu@gmail.com>
2025-04-09 17:06:19 -0600

    Document WelcomeText manager
    Document WelcomeText manager

--- Assets/Scripts/WelcomeUI/WelcomeTextManager.cs
+++ Assets/Scripts/WelcomeUI/WelcomeTextManager.cs
@@ -1,3 +1,13 @@
+/*
+ * WelcomeTextManager.cs
+ * 
+ * Description:
+ * This script controls the sequence of events when the player enters the game.
+ * The instructions are displayed in the welcome canvas UI. Once the player starts the game,
+ * a 5-minute countdown timer begins.
+ * The interactive game puzzles are hidden until the game starts.
+ */
+
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -12,23 +22,23 @@ public class WelcomeTextManager : MonoBehaviour
     public AudioSource narrative;  
     public AudioSource narrative2;  
 
-    //Hide interactive objects before game start  
+    //Array of objects to be hidden before the game starts
     public GameObject[] objectsToHide; 
     private GameObject remote; 
 
     private bool isFirstMessage = true;
-    private float timer = 300f; 
+    private float timer = 300f;  //Timer set to 5 minutes (300 seconds)
     private bool isTimerRunning = false;
 
     void Start()
     {
      
-        narrative.Play();
+        narrative.Play();                               // Start the introductory narrative audio
         nextButton.onClick.AddListener(ShowClueText);  
-        okButton.onClick.AddListener(HandleOkButton); 
+        okButton.onClick.AddListener(HandleOkButton);   // Add listener to the okButton to handle OK click
        
-        okButton.gameObject.SetActive(false);  
-        timerText.gameObject.SetActive(false);   
+        okButton.gameObject.SetActive(false);           // Hide the OK button initially
+        timerText.gameObject.SetActive(false);          // Hide the timer text initially
 
         // Hide all interactive objects before game start
         HideGameObject(); 
@@ -37,32 +47,32 @@ public class WelcomeTextManager : MonoBehaviour
         remote = FindRemote();
     }
 
+    // Show the first clue when the nextButton is clicked
     void ShowClueText()
     {
-        if (isFirstMessage)
+        if (isFirstMessage)     // Check if this is the first message
         {
-            narrative.Stop();
-            narrative2.Play();
-            welcomeText.text = "You have 5 minutes to escape!\n\nSomething important is on the TV!\n\nGood luck!";
-            nextButton.gameObject.SetActive(false);   
-            okButton.gameObject.SetActive(true);    
-            isFirstMessage = false;
+            narrative.Stop();   // Stop the first narrative audio
+            narrative2.Play();  // Play the second narrative audio
+            welcomeText.text = "You have 5 minutes to escape!\n\nSomething important is on the TV!\n\nGood luck!"; // Update the welcome text
+            nextButton.gameObject.SetActive(false);   // Hide the nextButton
+            okButton.gameObject.SetActive(true);      // Show the OK button
+            isFirstMessage = false;                   // Set the flag to false so the first message won't show again
         }
     }
 
     void Update()
     {
-        if (isTimerRunning)
+        if (isTimerRunning)             // Check if the timer is running
         {
-            timer -= Time.deltaTime; 
-            timerText.text = "Time remaining: " + FormatTime(timer);
+            timer -= Time.deltaTime;    // Decrease the timer by the time passed since last frame
+            timerText.text = "Time remaining: " + FormatTime(timer);    // Update the timer text UI
 
-            if (timer <= 0)
+            if (timer <= 0)             // If the timer reaches zero
             {
-                timer = 0;  
-                isTimerRunning = false;
-                GameOver();
-               
+                timer = 0;              // Set the timer to zero
+                isTimerRunning = false; // Stop the timer
+                GameOver();             // Call the GameOver function
             }
 
         }
@@ -73,15 +83,14 @@ public class WelcomeTextManager : MonoBehaviour
         okButton.gameObject.SetActive(false);  
         welcomeText.gameObject.SetActive(false);
         timerText.gameObject.SetActive(true);
-        isTimerRunning = true;
+        isTimerRunning = true;      // Start the timer
 
         if (timerAudio != null)
         {
-            timerAudio.Play();
+            timerAudio.Play();      // Play the timer countdown audio
         }
 
-        //Enable Remote: First Clue
-        remote.SetActive(true);
+        remote.SetActive(true);      //Enable Remote: First Clue
 
     }
 
@@ -93,35 +102,37 @@ public class WelcomeTextManager : MonoBehaviour
         return string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
+    // Called when the game is over (timer reaches zero)
     void GameOver()
     {
       
         if (timerAudio != null)
         {
-            timerAudio.Stop();
+            timerAudio.Stop();          // Stop the timer audio
         }
 
    
-        timerText.text = "Game Over!";
+        timerText.text = "Game Over!"; // Display "Game Over" message on the timer UI
     }
 
+    // Hide all objects in the objectsToHide array
     void HideGameObject(){
         foreach (GameObject obj in objectsToHide)
         {
-            obj.SetActive(false); 
+            obj.SetActive(false);  // Set each object as inactive
         }  
     }
 
-    GameObject FindRemote()
+    GameObject FindRemote()     // Find the remote object in the objectsToHide array
     {
         foreach (GameObject obj in objectsToHide)
         {
-            if (obj.name.ToLower().Contains("remote")) 
+            if (obj.name.ToLower().Contains("remote"))  // Check if the object's name contains "remote"
             {
-                return obj;
+                return obj;     // Return the remote object
             }
         }
-        return null;
+        return null;            // Return null if no remote object is found
     }
 
 }
